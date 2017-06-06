1e53b8c824acba2b7bc37acd0a9df1481aceb798
root <root@zabbixcom.net.eximb.com>
2017-06-06 16:57:31 +0300

    Changes

--- src/TurbosmsServiceProvider.php
+++ src/TurbosmsServiceProvider.php
@@ -7,16 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class TurbosmsServiceProvider extends ServiceProvider
 {
     /**
-<<<<<<< HEAD
-=======
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
-
-    /**
->>>>>>> 2d459bb11f62c37fecf1a1ab345a002f595a288b
     * Perform post-registration booting of services.
     *
     * @return void
@@ -43,10 +33,6 @@ class TurbosmsServiceProvider extends ServiceProvider
         * Register main turbosms service
         */
         $this->app->bind('laravel-turbosms', function($app) {
-<<<<<<< HEAD
-=======
-            $confer = 1;
->>>>>>> 2d459bb11f62c37fecf1a1ab345a002f595a288b
             return new Turbosms();
         });
     }
